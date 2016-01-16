210edaed7b8b70df018667f8c5b3a3b7ed2c7c4f
Peter Duggins <psipeter@gmail.com>
2016-01-16 17:21:31 -0500

    worked on ReLUneuron

--- SYDE556-750Assignment1.py
+++ SYDE556-750Assignment1.py
@@ -3,7 +3,22 @@
 # Jan 25, 2015
 # Assignment 1
 
-class RLneuron():
+import numpy as np
 
-	def __init__(self):
-		self.
+class ReLUneuron():
+
+	def __init__(self,x_intercept,max_firing_rate):
+		self.xintercept=x_intercept
+		self.maxrate=max_firing_rate
+		self.alpha = (self.maxrate - 0)/(1 - self.xintercept)	#alpha=slope=(y2-y1)/(x2-x1)
+
+	def rate(self,J):
+		b=-self.xintercept*self.alpha	
+		rate=self.alpha*J+b	
+		return np.max(0,rate)
+
+	def Jbias(self):
+		return self.xintercept
+
+	def alpha(self):
+		return self.xintercept
